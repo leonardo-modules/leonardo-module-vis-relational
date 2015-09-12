4bdf5f21975624468396e0ae362550a2f027760e
Ales Komarek <mail@newt.cz>
2015-09-12 12:18:50 +0200

    icicle diagram fixes

--- leonardo_module_vis_relational/static/vis/js/icicle.js
+++ leonardo_module_vis_relational/static/vis/js/icicle.js
@@ -7,18 +7,19 @@ function icicle(config) {
   var color = d3.scale.category20();
 
   var svg = d3.select(config.placeholder).append("svg")
-      .attr("width", width)
-      .attr("height", height);
+    .attr("width", width)
+    .attr("height", height);
 
   var partition = d3.layout.partition()
-      .size([width, height])
-      .value(function(d) { return d.size; });
+    .size([width, height])
+    .value(function(d) { return d.size; });
 
   d3.json(config.data_source, function(error, root) {
+
     var nodes = partition.nodes(root);
 
     svg.selectAll(".node")
-        .data(nodes)
+      .data(nodes)
       .enter().append("rect")
         .attr("class", "node")
         .attr("x", function(d) { return d.x; })
@@ -28,10 +29,10 @@ function icicle(config) {
         .style("fill", function(d) { return color((d.children ? d : d.parent).name); });
 
     svg.selectAll(".label")
-        .data(nodes.filter(function(d) { return d.dx > 6; }))
+      .data(nodes.filter(function(d) { return d.dx > 6; }))
       .enter().append("text")
         .attr("class", "label")
-        .attr("dy", ".35em")
+        .attr("dy", "0em")
         .attr("transform", function(d) { return "translate(" + (d.x + d.dx / 2) + "," + (d.y + d.dy / 2) + ")rotate(90)"; })
         .text(function(d) { return d.name; });
   });
